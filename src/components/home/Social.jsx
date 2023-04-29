9e9ab305550d5d7f2541c383322595f11aa4acf6
Natalie <79932225+Nadia982@users.noreply.github.com>
2023-04-29 16:35:03 +0100

    added home section, social icons and photo animation

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/Nadia982"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/natalie-g-b947331a/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+export default Social;
